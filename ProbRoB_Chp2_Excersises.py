2c8b7790cd277c3403823c5855971dfa2a67b0c1
Alex Garrow <al.garrow@yahoo.com>
2025-05-24 00:00:08 -0400

    finished remainder of exercise 2 and started on exercise 3

--- ProbRoB_Chp2_Excersises.py
+++ ProbRoB_Chp2_Excersises.py
@@ -13,11 +13,12 @@ Exercise 2.b
 Random Weather Sequence Generator
 The weather can be either sunny, cloudy, or rainy.
 """
-
+print("\nExercise Questions from Chapter 2 of Probabilistic Robotics")
+print("\n\n Exercise 2.b \n")
 
 # Outcomes and their probabilities
 outcomes = ["Sunny", "Cloudy", "Rainy"] # outcomes of weather to randomly select from
-probabilities = [0.4667, 0.4, 0.1333]  # Probabilities must sum to 1, probability of each outcome
+probabilities = [0.6429, 0.2857, 0.0714]  # Probabilities must sum to 1, probability of each outcome
 # I came up with these values by summing the chance it was sunny based on all 3 previous possible days 
 # and then dividing by the total sum of sunny, rainy, cloudy
 
@@ -47,13 +48,18 @@ for i in range(10): # days shortened to 10 for code run time, for statistical si
 
  Weather_List.append(Weather_tomorrow) # adds the weather tomorrow selected by the if-elif statements above
 
-#print("Weather Sequence: ", Weather_List) # used to verify testing
+print("Weather Sequence: ", Weather_List) # used to verify testing
+
+
+
+
 
 """
 Exercise 2.c
 Calculate the stationary distribution of this markov chain
 
 """
+print("\n\n Exercise 2.c \n")
 
 # initialize the sums
 Sunny_Sum = 0
@@ -84,12 +90,16 @@ print("Probability of Sunny: ", Probability_Sunny)
 print("Probability of Cloudy: ", Probability_Cloudy)
 print("Probability of Rainy: ", Probability_Rainy)
 
+
+
+
+
 """
 Excersise 2.d
 What is th estationary distribution based on the state transition matrix?
 Did this one by hand, may come back to solve with system of equations functions in python later
 """
-
+print("\n\n Exercise 2.d \n")
 # equations written out by hand in my notebook using this as an
 # opportunity to practice solving systems of equations in python
 A = np.array([
@@ -104,13 +114,82 @@ print("Probability Sunny Closed form Solution:", solution[0])
 print("Probability Cloudy Closed form Solution:", solution[1])
 print("Probability Rainy Closed form Solution:", solution[2])
 
+
+
+
+
 """
 Exerise 2.e
 What is the entropy of the stationary distribution
 """
+print("\n\n Exercise 2.e \n")
 Probability_Sunny = 0.6429
 Probability_Cloudy = 0.2857
 Probability_Rainy = 0.0714
 
 Hp = - (Probability_Sunny * np.log2(Probability_Sunny) + Probability_Cloudy * np.log2(Probability_Cloudy) + Probability_Rainy * np.log2(Probability_Rainy))
 print("Entropy of the Stationary distribution: ", Hp)
+
+
+
+
+
+"""
+Exercise 2.f
+using bayes rule compute the probability table of yesterday's weather given todays weather
+"""
+print("\n\n Exercise 2.f \n")
+prob_sunY_sunT = 0.8*Probability_Sunny/Probability_Sunny
+prob_sunY_cloudT = 0.2*Probability_Sunny/Probability_Cloudy
+prob_sunY_rainT = 0*Probability_Sunny/Probability_Rainy
+
+prob_cloudY_sunT = 0.4*Probability_Cloudy/Probability_Sunny
+prob_cloudY_cloudT = 0.4*Probability_Cloudy/Probability_Cloudy
+prob_cloudY_rainT = 0.2*Probability_Cloudy/Probability_Rainy
+
+prob_rainY_sunT = 0.2*Probability_Rainy/Probability_Sunny
+prob_rainY_cloudT = 0.6*Probability_Rainy/Probability_Cloudy
+prob_rainY_rainT = 0.2*Probability_Rainy/Probability_Rainy
+
+ProbMatrix = np.array([[prob_sunY_sunT, prob_sunY_cloudT, prob_sunY_rainT],
+                       [prob_cloudY_sunT, prob_cloudY_cloudT, prob_cloudY_rainT],
+                       [prob_rainY_sunT, prob_rainY_cloudT, prob_rainY_rainT]])
+
+print("Probability Matrix: \n", ProbMatrix)
+
+
+"""
+Exercise 3
+Suppose we cannot measure weather directly, but instead we rely on a sensor. The problem is that our sensor is noisy.
+ Its measurements are goverened by the following measurement model
+"""
+measurement_model = np.array([
+    [0.6, 0.4, 0.0],
+    [0.3, 0.7, 0.0],
+    [0.0, 0.0, 1.0]
+])
+
+weather_stm = np.array([
+    [0.8, 0.2, 0.0],
+    [0.4, 0.4, 0.2],
+    [0.2, 0.6, 0.2]
+])
+
+
+
+"""
+Exercise 3.a
+suppose day 1 is sunny (known as a fact) and in the following 4 days the sensor measures cloudy, cloudy, rainy, sunny.
+What is the probability it is sunny on day 5?
+"""
+print("\n\n Exercise 3.a \n")
+
+print(np.multiply(np.array([1, 2, 3]),np.array([1,2,3])))
+print(measurement_model[:, 0])
+
+"""
+belief = np.array([1, 0, 0]) # initial belief state
+measurement = np.array([2, 2, 3, 1]) # sensor measurements (cloudy, cloudy, rainy, sunny)
+
+for z in measurement:
+"""
